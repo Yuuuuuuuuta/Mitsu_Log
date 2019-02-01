ed94e6511608b42f859f17b701c4fc91ffd0afc4
Yuuuuuuuuta <y.mitsu92@gmail.com>
2019-02-01 22:31:30 +0900

    modelに登録完了

--- src/main/java/com/example/demo/login/domain/service/UserService.java
+++ src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -3,6 +3,7 @@ package com.example.demo.login.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.repository.UserDao;
 
 @Service
@@ -10,5 +11,19 @@ public class UserService {
 
 	@Autowired
 	UserDao dao;
+
+	public boolean insert(User user) {
+
+		int rowNumber = dao.insertOne(user);
+
+		boolean result = false;
+
+		if (rowNumber > 0) {
+			result = true;
+		}
+
+		return result;
+
+	}
 
 }

8714410ea1d8e4b4c81e5fb22a3b32cb3d8d948e
Yuuuuuuuuta <y.mitsu92@gmail.com>
2019-02-04 00:10:47 +0900

    パスワード暗号化実装

--- src/main/java/com/example/demo/login/domain/service/UserService.java
+++ src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Service;
 import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.repository.UserDao;
 
-
-
 @Service
 public class UserService {
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 --アドミン権限
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
-VALUES('y.mitsu92@gmail.com','P@ssw0rd', '三橋裕太','1992-09-02',26,true,'ROLE_ADMIN');
+VALUES('y.mitsu92@gmail.com','$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '三橋裕太','1992-09-02',26,true,'ROLE_ADMIN');
 
 --一般権限
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
-VALUES('sample@xxx.co.jp','password','テストユーザー','1990-10-01',28,false,'ROLE_GENERAL');
+VALUES('sample@xxx.co.jp','$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa','テストユーザー','1990-10-01',28,false,'ROLE_GENERAL');

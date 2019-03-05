a81cfee87a0ee00214d61605c03f4fc985d469c3
Yuuuuuuuuta <y.mitsu92@gmail.com>
2019-03-05 22:15:52 +0900

    REST/POSTメソッド追加完了

--- src/main/java/com/example/demo/login/controller/UserRestController.java
+++ src/main/java/com/example/demo/login/controller/UserRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.login.domain.model.User;
@@ -28,7 +30,21 @@ public class UserRestController {
 		return service.selectOne(userId);
 	}
 
+	@PostMapping("/rest/insert")
+	public String postUserOne(@RequestBody User user) {
 
+		boolean result = service.insert(user);
 
+		String str = "";
+
+		if(result == true) {
+			str = "{\"result\":\"ok\"}";
+		}else {
+			str = "{\"result\":\"error\"}";
+		}
+
+		return str;
+
+	}
 
 }

d38320504f314b0da396fbb04df819e8c0776781
Yuuuuuuuuta <y.mitsu92@gmail.com>
2019-03-06 20:59:31 +0900

    DELETEメソッド完了

--- src/main/java/com/example/demo/login/domain/service/jdbc/RestServiceJdbcImpl.java
+++ src/main/java/com/example/demo/login/domain/service/jdbc/RestServiceJdbcImpl.java
@@ -55,8 +55,17 @@ public class RestServiceJdbcImpl implements RestService{
 		}
 	}
 
+	@Override
 	public boolean delete(String userId) {
-		return false;
+
+		int result = dao.deleteOne(userId);
+
+		if(result == 0) {
+			return false;
+		}else {
+			return true;
+		}
+
 	}
 
 
